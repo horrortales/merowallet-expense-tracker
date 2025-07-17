32410f4ea7cc031e8185d30ef158180e2d5d98e0
horrortales <horrortales30@gmail.com>
2025-07-17 21:14:32 +0545

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { AuthProvider } from "../contexts/AuthContext";
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }}>
